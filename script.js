433e45025ae028bab9b7f16aa88f130e9ec973f8
jericjan <cantilfrederick@gmail.com>
2023-08-02 14:34:21 +0800

    code cleanup
    more logs
    
    more logsss

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ document.getElementById("generate").onclick = function () {
   generateAlbum();
 };
 
+document.getElementById("go").onclick = function () {
+  LinkGet(document.getElementById('data'));
+};
+
 function generateAlbum() {
   var finaltitle = document.getElementById("title").value.split("\n")[0].trim();
   var finalartist = document
@@ -58,13 +62,6 @@ function downloadThing() {
       });
   }
 
-  var client = new XMLHttpRequest();
-
-  var fd = new FormData();
-  urltoFile(file.src, "img.png", "image/png").then(function (file) {
-    fd.append("file", file);
-  });
-
   function saveBlob(blob, fileName) {
     var a = document.createElement("a");
     a.href = window.URL.createObjectURL(blob);
@@ -72,128 +69,113 @@ function downloadThing() {
     a.dispatchEvent(new MouseEvent("click"));
   }
 
-  function checkFlag() {
-    if (fd.get("file") == null) {
-      window.setTimeout(
-        checkFlag,
-        100
-      ); /* this checks the flag every 100 milliseconds*/
-    } else {
-      console.log(fd.get("file"));
-      $.ajax({
-        xhr: function () {
-          var xhr = new window.XMLHttpRequest();
+  var fd = new FormData();
+  urltoFile(file.src, "img.png", "image/png").then(function (file) {
+    fd.append("file", file);
+    
+    $.ajax({
+      xhr: function () {
+        var xhr = new window.XMLHttpRequest();
+        var evtSource;
 
-          // Upload progress
-          xhr.upload.addEventListener(
-            "progress",
-            function (evt) {
-              if (evt.lengthComputable) {
-                var percentComplete = (evt.loaded / evt.total) * 100;
-                //Do something with upload progress
-                console.log(percentComplete);
-                document.getElementById("status").innerHTML =
-                  percentComplete.toFixed(1) + "% uploaded";
-                $(".progress-bar2").each(function () {
-                  var width = percentComplete.toFixed(1) + "%";
-                  $(this).animate({ width: width }, 1);
-                });
-                if (
-                  document.getElementById("status").innerHTML ==
-                  "100.0% uploaded"
-                ) {
-                  document.getElementById("status").innerHTML =
-                    "Doing magic... Give it a minute or two.";
 
-                  var evtSource = new EventSource(
-                    "https://yt2mp3-albumart.jericjanjan.repl.co/log?pogid=" +
-                      uuid,
-                    { withCredentials: true }
-                  );
-                  window.evtsource = evtSource;
-                  evtSource.onmessage = function (e) {
-                    if (e.data == "Beep boop...") {
-                      document.getElementById("log").innerHTML = e.data;
-                    } else {
-                      document.getElementById("log").innerHTML = e.data;
-                      document.getElementById("status").innerHTML =
-                        "Processing...";
-                      $(".progress-bar2").each(function () {
-                        var width = e.data.split("(")[1].split(")")[0];
-                        $(this).animate({ width: width }, 1);
-                      });
-                    }
 
-                    //		}
-                  };
-                }
+        // Upload progress
+        xhr.upload.addEventListener(
+          "progress",
+          function (evt) {
+            if (evt.lengthComputable) {              
+              var logElem = document.getElementById("log")
+              var statusElem = document.getElementById("status")
+              var percentComplete = ((evt.loaded / evt.total) * 100).toFixed(1) + "%";
+              //Do something with upload progress
+              console.log(percentComplete, " Upload progress listener triggered.");
+              document.getElementById("status").innerHTML =
+                percentComplete + " uploaded";
+              $(".progress-bar2").animate({ width: percentComplete }, 1);
+              if (statusElem.innerHTML == "100.0% uploaded") {
+                statusElem.innerHTML =
+                  "Doing magic... Give it a minute or two.";
+                console.log("100% hit. Starting EventSource");
+                evtSource = new EventSource(
+                  "https://yt2mp3-albumart.jericjanjan.repl.co/log?pogid=" +
+                    uuid,
+                  { withCredentials: true }
+                );
+                evtSource.onmessage = function (e) {
+                  console.log("EventSource received a msg");
+                  logElem.innerHTML = e.data;
+                  if (e.data != "Beep boop...") {                    
+                    statusElem.innerHTML = "Processing...";
+
+                    var width = e.data.split("(")[1].split(")")[0];
+                    $(".progress-bar2").animate({ width: width }, 1);
+                    
+                  }
+                };
               }
-            },
-            false
-          );
+            }
+          },
+          false
+        );
 
-          // Download progress
-          xhr.addEventListener(
-            "progress",
-            function (evt) {
-              if (evt.lengthComputable) {
-                evtsource.close();
-                document.getElementById("log").innerHTML = "";
-                var percentComplete = (evt.loaded / evt.total) * 100;
-                // Do something with download progress
-                console.log(percentComplete);
-                document.getElementById("status").innerHTML =
-                  percentComplete.toFixed(1) + "% downloaded";
-                $(".progress-bar2").each(function () {
-                  var width = percentComplete.toFixed(1) + "%";
-                  $(this).animate({ width: width }, 1);
-                });
-                if (
-                  document.getElementById("status").innerHTML ==
-                  "100.0% downloaded"
-                ) {
-                  document.getElementById("status").innerHTML = "FINISHED!";
-                }
+        // Download progress
+        xhr.addEventListener(
+          "progress",
+          function (evt) {
+            if (evt.lengthComputable) {
+              evtSource.close();
+              document.getElementById("log").innerHTML = "";
+              var percentComplete = ((evt.loaded / evt.total) * 100).toFixed(1) + "%";
+              // Do something with download progress
+              console.log(percentComplete);
+              var statusElem = document.getElementById("status")
+              statusElem.innerHTML = percentComplete + " downloaded";              
+              $(".progress-bar2").animate({ width: percentComplete }, 1);              
+              if (statusElem.innerHTML =="100.0% downloaded") {
+                statusElem.innerHTML = "FINISHED!";
               }
-            },
-            false
-          );
+            }
+          },
+          false
+        );
 
-          return xhr;
-        },
-        type: "POST",
-        url:
-          "https://yt2mp3-albumart.jericjanjan.repl.co/download?url=" +
-          url +
-          "&author=" +
-          finalartist +
-          "&title=" +
-          finaltitle +
-          "&album=" +
-          finalalbum +
-          "&uuid=" +
-          uuid,
-        data: fd,
-        processData: false,
-        contentType: false,
-        xhrFields: {
-          responseType: "blob",
-          withCredentials: true,
-        },
-        success: function (blob) {
-          console.log(blob);
-          console.log(typeof blob);
-          saveBlob(blob, title + ".mp3");
-        },
-        error: function (xhr, status, error) {
-          var errorMessage = xhr.status + ": " + xhr.statusText;
-          document.getElementById("status").innerHTML =
-            errorMessage + " Try again :(";
-        },
-      });
-    }
-  }
-  checkFlag();
+        return xhr;
+      },
+      type: "POST",
+      url:
+        "https://yt2mp3-albumart.jericjanjan.repl.co/download?url=" +
+        url +
+        "&author=" +
+        finalartist +
+        "&title=" +
+        finaltitle +
+        "&album=" +
+        finalalbum +
+        "&uuid=" +
+        uuid,
+      data: fd,
+      processData: false,
+      contentType: false,
+      xhrFields: {
+        responseType: "blob",
+        withCredentials: true,
+      },
+      success: function (blob) {
+        console.log(blob);
+        console.log(typeof blob);
+        saveBlob(blob, title + ".mp3");
+      },
+      error: function (xhr, status, error) {
+        var errorMessage = xhr.status + ": " + xhr.statusText;
+        document.getElementById("status").innerHTML =
+          errorMessage + " Try again :(";
+      },
+    });
+  });
+
+
+
 }
 
 var animateButton = function (e) {
@@ -209,62 +191,28 @@ var animateButton = function (e) {
 
 var bubblyButtons = document.getElementsByClassName("bubbly-button");
 
-for (var i = 0; i < bubblyButtons.length; i++) {
-  bubblyButtons[i].addEventListener("click", animateButton, false);
-}
-
-function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-function invertColor(hex) {
-  if (hex.indexOf("#") === 0) {
-    hex = hex.slice(1);
-  }
-  // convert 3-digit hex to 6-digits.
-  if (hex.length === 3) {
-    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-  }
-  if (hex.length !== 6) {
-    throw new Error("Invalid HEX color.");
-  }
-  // invert color components
-  var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
-    g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
-    b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
-  // pad each with zeros and return
-  return "#" + padZero(r) + padZero(g) + padZero(b);
-}
-
-function padZero(str, len) {
-  len = len || 2;
-  var zeros = new Array(len).join("0");
-  return (zeros + str).slice(-len);
-}
-
-function set_image() {
-  var el = document.getElementById("first");
-  var resize = new Croppie(el, {
-    viewport: { width: 200, height: 200 },
-    boundary: { width: 600, height: 600 },
-    showZoomer: true,
-    enableResize: false,
-    enableOrientation: true,
-    mouseWheelZoom: "ctrl",
-    useCORS: true,
-  });
-  resize.bind({
-    url: el,
-  });
-  window.resize = resize;
+for (const bubblyButton of bubblyButtons) {
+  bubblyButton.addEventListener("click", animateButton, false);
 }
 
 function LinkGet(val) {
+  function set_image() {
+    var el = document.getElementById("first");
+    var resize = new Croppie(el, {
+      viewport: { width: 200, height: 200 },
+      boundary: { width: 600, height: 600 },
+      showZoomer: true,
+      enableResize: false,
+      enableOrientation: true,
+      mouseWheelZoom: "ctrl",
+      useCORS: true,
+    });
+    resize.bind({
+      url: el,
+    });
+    window.resize = resize;
+  }
+
   if (window.resize == undefined) {
     console.log("no Croppie yet");
   } else {
@@ -292,22 +240,13 @@ function LinkGet(val) {
     .then((json) => {
       console.log("parsed json", json); // access json.body here
       var thumbnail_url = json.items[0].snippet.thumbnails;
-      if (thumbnail_url.maxres !== undefined) {
-        var final_url = thumbnail_url.maxres.url;
-        console.log(final_url);
-      } else if (thumbnail_url.standard !== undefined) {
-        var final_url = thumbnail_url.standard.url;
-        console.log(final_url);
-      } else if (thumbnail_url.high !== undefined) {
-        var final_url = thumbnail_url.high.url;
-        console.log(final_url);
-      } else if (thumbnail_url.medium !== undefined) {
-        var final_url = thumbnail_url.medium.url;
-        console.log(final_url);
-      } else if (thumbnail_url.default !== undefined) {
-        var final_url = thumbnail_url.default.url;
-        console.log(final_url);
-      }
+      var final_url = thumbnail_url.maxres ? thumbnail_url.maxres.url :
+        thumbnail_url.standard ? thumbnail_url.standard.url :
+        thumbnail_url.high ? thumbnail_url.high.url :
+        thumbnail_url.medium ? thumbnail_url.medium.url :
+        thumbnail_url.default.url
+      console.log(final_url);
+
       var pic = document.getElementById("first");
       pic.crossOrigin = "Anonymous";
       pic.src =
@@ -316,17 +255,50 @@ function LinkGet(val) {
       window.title = json.items[0].snippet.title;
       window.author = json.items[0].snippet.channelTitle;
 
-      // Make sure image is finished loading
-      if (img.complete) {
+      function addAnims(){
+        function componentToHex(c) {
+          var hex = c.toString(16);
+          return hex.length == 1 ? "0" + hex : hex;
+        }        
+        function rgbToHex(r, g, b) {
+          return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+        }
+        function invertColor(hex) {
+          function padZero(str, len) {
+            len = len || 2;
+            var zeros = new Array(len).join("0");
+            return (zeros + str).slice(-len);
+          }
+        
+          if (hex.indexOf("#") === 0) {
+            hex = hex.slice(1);
+          }
+          // convert 3-digit hex to 6-digits.
+          if (hex.length === 3) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+          }
+          if (hex.length !== 6) {
+            throw new Error("Invalid HEX color.");
+          }
+          // invert color components
+          var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+            g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+            b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+          // pad each with zeros and return
+          return "#" + padZero(r) + padZero(g) + padZero(b);
+        }
+
+
         console.log(colorThief.getColor(img));
         var color = colorThief.getColor(img);
+        var animOptions = { duration: 1000, queue: false }
 
         $("body").animate(
           {
             backgroundColor:
-              "RGB(" + color[0] + "," + color[1] + "," + color[2] + ")",
+              `RGB(${color[0]},${color[1]},${color[2]})`,
           },
-          { duration: 1000, queue: false }
+          animOptions
         );
         $("textarea").animate(
           {
@@ -334,75 +306,38 @@ function LinkGet(val) {
               rgbToHex(color[0], color[1], color[2])
             ),
           },
-          { duration: 1000, queue: false }
+          animOptions
         );
         $("textarea").animate(
-          { color: "RGB(" + color[0] + "," + color[1] + "," + color[2] + ")" },
-          { duration: 1000, queue: false }
+          { color: `RGB(${color[0]},${color[1]},${color[2]})` },
+          animOptions
         );
         $("h3").animate(
           { color: invertColor(rgbToHex(color[0], color[1], color[2])) },
-          { duration: 1000, queue: false }
+          animOptions
         );
         $("div#status,div#log").animate(
           { color: invertColor(rgbToHex(color[0], color[1], color[2])) },
-          { duration: 1000, queue: false }
+          animOptions
         );
         $("img#final").animate(
           {
             "border-color": invertColor(rgbToHex(color[0], color[1], color[2])),
           },
-          { duration: 1000, queue: false }
+          animOptions
         );
         $("img#final").animate(
           { "border-width": "8px" },
-          { duration: 1000, queue: false }
+          animOptions
         );
+      }
+
+      // Make sure image is finished loading
+      if (img.complete) {
+        addAnims()
       } else {
         img.addEventListener("load", function () {
-          console.log(colorThief.getColor(img));
-          var color = colorThief.getColor(img);
-          $("body").animate(
-            {
-              backgroundColor:
-                "RGB(" + color[0] + "," + color[1] + "," + color[2] + ")",
-            },
-            { duration: 1000, queue: false }
-          );
-          $("textarea").animate(
-            {
-              backgroundColor: invertColor(
-                rgbToHex(color[0], color[1], color[2])
-              ),
-            },
-            { duration: 1000, queue: false }
-          );
-          $("textarea").animate(
-            {
-              color: "RGB(" + color[0] + "," + color[1] + "," + color[2] + ")",
-            },
-            { duration: 1000, queue: false }
-          );
-          $("h3").animate(
-            { color: invertColor(rgbToHex(color[0], color[1], color[2])) },
-            { duration: 1000, queue: false }
-          );
-          $("div#status,div#log").animate(
-            { color: invertColor(rgbToHex(color[0], color[1], color[2])) },
-            { duration: 1000, queue: false }
-          );
-          $("img#final").animate(
-            {
-              "border-color": invertColor(
-                rgbToHex(color[0], color[1], color[2])
-              ),
-            },
-            { duration: 1000, queue: false }
-          );
-          $("img#final").animate(
-            { "border-width": "8px" },
-            { duration: 1000, queue: false }
-          );
+          addAnims()
         });
       }
 
@@ -421,8 +356,7 @@ document.getElementById("update-ytdlp").onclick = function () {
   document.querySelector("#ytdlp-log").innerHTML = "";
   var evtSource = new EventSource(
     "https://yt2mp3-albumart.jericjanjan.repl.co/update-yt-dlp"
-  );
-  window.evtsource = evtSource;
+  );  
   evtSource.onmessage = function (e) {
     if (e.data == "END") {
       console.log("closing");
@@ -466,25 +400,27 @@ $(".js-modal").on("click", function (e) {
 });
 // ^^ jquery Multi Modal Control - https://codepen.io/hnjungElis/pen/ZEyzPbe
 
-window.addEventListener("load", function () {
-  var http = new XMLHttpRequest();
-  http.open(
-    "POST",
-    "https://api.uptimerobot.com/v2/getMonitors?format=json&api_key=m789063836-d171e621fd1ec06c641b4942",
-    false
-  );
-  http.send();
-  var status = JSON.parse(http.response).monitors[0].status;
-  if (status == "0") {
-    var fstatus = "PAUSED";
-  } else if (status == "1") {
-    var fstatus = "NOT CHECKED YET";
-  } else if (status == "2") {
-    var fstatus = "UP";
-  } else if (status == "8") {
-    var fstatus = "SEEMS DOWN";
-  } else if (status == "9") {
-    var fstatus = "DOWN";
-  }
-  document.querySelector("#uptime2").innerHTML = fstatus;
+window.addEventListener("load", function() {
+  setTimeout(function () {
+    var http = new XMLHttpRequest();
+    http.open(
+      "POST",
+      "https://api.uptimerobot.com/v2/getMonitors?format=json&api_key=m789063836-d171e621fd1ec06c641b4942",
+      false
+    );
+    http.send();
+    var status = JSON.parse(http.response).monitors[0].status;
+    if (status == "0") {
+      var fstatus = "PAUSED";
+    } else if (status == "1") {
+      var fstatus = "NOT CHECKED YET";
+    } else if (status == "2") {
+      var fstatus = "UP";
+    } else if (status == "8") {
+      var fstatus = "SEEMS DOWN";
+    } else if (status == "9") {
+      var fstatus = "DOWN";
+    }
+    document.querySelector("#uptime2").innerHTML = fstatus;
+  }, 0 )
 });
